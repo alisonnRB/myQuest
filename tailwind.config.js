7191219d2845e9bf92c6c1d03f690c5ac963439d
alisonnRB <alisonrbjs@gmail.com>
2024-04-13 01:02:47 -0300

    tailwind and colors of itself created

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        error: "#E16868",
+        right: "#6DB154",
+        firstBg: "#162238",
+        secondBg: "#343959",
+        thirdBg: "#69709A"
+      }
+    },
+  },
+  plugins: [],
+}
+
