f8759f5980a5e5b9cec527903fa5f418201a8341
alisonnRB <alisonrbjs@gmail.com>
2024-04-13 01:47:40 -0300

    right for init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HelloComponent } from "./hello/hello.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HelloComponent]
+    imports: [RouterOutlet]
 })
 export class AppComponent {
   title = 'projeto';
