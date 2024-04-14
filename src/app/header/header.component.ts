bfe18607ba99fe15e92a05d1a1ed2833814e4c7a
alisonnRB <alisonrbjs@gmail.com>
2024-04-14 03:17:07 -0300

    resquest and convesion by api created

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,68 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import axios, { AxiosResponse } from 'axios';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
 
+export class HeaderComponent {
+  Tema: string = '';
+  dificuldade: string = 'facil';
+  quests: Object = {};
+  trys: number = 0;
+
+  async Submit() {
+
+    const headers = {
+      'Content-Type': 'application/json',
+    };
+
+    const body = {
+      "contents": [
+        {
+          "parts": [
+            { "text": `Escreva 5 perguntas de múltipla escolha sobre o tema ${this.Tema} com nível de dificuldade ${this.dificuldade} tendo 4 opções cada pergunta, mas estruture como um JSON. Exemplo de saída: 'pergunta1': {'questao': 'pergunta', 'opcao1': 'opcao', 'resposta': 'opcao correta'}` }
+          ]
+        }
+      ]
+    };
+
+    try {
+      const resposta: AxiosResponse = await axios.post('https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=AIzaSyD9o9gxXs1OFiTco7K6_nwYXmARptbdqXg', body, { headers });
+      this.JsonTransforme(resposta.data.candidates[0].content.parts[0].text);
+    } catch (error) {
+      console.error('Erro ao fazer a requisição:', error);
+    }
+  }
+
+  private JsonTransforme(text: string) {
+    try {
+
+      let strFiltred = text.replace(/json/, '');
+      const RespostInJson = JSON.parse(strFiltred);
+      this.quests = RespostInJson;
+      this.trys = 0;
+
+      console.log('certo');
+
+    } catch (e) {
+
+      if (this.trys < 2) {
+        this.trys = this.trys + 1;
+        this.Submit();
+      } else {
+        console.log('erro');
+      }
+
+    }
+
+  }
+  
 }
