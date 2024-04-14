2b3d3fed6557aaeaeea52c0ab40375f9e92a194f
alisonnRB <alisonrbjs@gmail.com>
2024-04-14 08:41:47 -0300

    created body

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    BodyComponent
+  ]
 })
 export class AppComponent {
   title = 'projeto';
