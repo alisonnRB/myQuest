61cf9065ecaeb05f779dd9c2537685c979d078be
alisonnRB <alisonrbjs@gmail.com>
2024-04-15 06:45:35 -0300

    verificação

--- src/app/service/Persistence.ts
+++ src/app/service/Persistence.ts
@@ -9,9 +9,9 @@ class Persistence {
         localStorage.setItem('data', JSON.stringify(data));
     }
 
-    answerPersistece(index: number, done : boolean = false, right: boolean = false, answer: number = 0){
+    answerPersistece(index: number, done: boolean = false, right: boolean = false, answer: number = 0) {
         const data: any = this.getPersistence();
-        if(data){
+        if (data) {
             const keys = Object.keys(data.quests);
             data.quests[keys[index - 1]].zDone = done;
             data.quests[keys[index - 1]].zRight = right;
@@ -22,15 +22,15 @@ class Persistence {
     }
 
 
-    getPersistence() {
-        const data: any = localStorage.getItem('data');
-        if (data) {
+    getPersistence(): any {
+        if (typeof localStorage !== 'undefined') {
+          const data: any = localStorage.getItem('data');
+          if (data !== null && data !== undefined) {
             return JSON.parse(data);
-        } else {
-            return false;
+          }
         }
-
-    }
+        return null;
+      }
 }
 
 export default Persistence;
